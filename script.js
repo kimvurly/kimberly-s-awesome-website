e575b3de7cd930d20a8bdb2d2c9422984798d242
kimberly vu <kimberlyy.vuu@gmail.com>
2025-06-15 17:17:49 -0700

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var leftColumn = document.querySelector('.left');
+var logo = document.querySelector('.logo');
+
+if (leftColumn.style.display === 'none'){
+    leftColumn.style.display = 'block';
+}
