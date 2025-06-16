113221fa0d3b95480bac8f04931e518a828dc1d6
kimberly vu <kimberlyy.vuu@gmail.com>
2025-06-15 17:35:57 -0700

    commit

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var leftColumn = document.querySelector('.left');
-var logo = document.querySelector('.logo');
+const leftColumn = document.getElementsByClassName("column left");
+const viewportWidth = window.innerWidth;
 
-if (leftColumn.style.display === 'none'){
-    leftColumn.style.display = 'block';
-}
+document.addEventListener('scroll', ()=> {
+    const top = document.documentElement.scrollTop;
+    if (viewportWidth <= 1060){
+        if (top > 10){
+            leftColumn[0].style.backgroundColor ="#d5e4c1";
+            leftColumn[0].style.boxShadow = "5px 5px 10px 2px #736c64";
+        }
+        else {
+            leftColumn[0].style.backgroundColor ="#ebf8ff";
+            leftColumn[0].style.boxShadow = "none";
+        }
+    }
+})
